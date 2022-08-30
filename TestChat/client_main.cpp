37c8d9a98b16c438d4d24156c5f6926597687241
Vincenzo Caliendo <82732029+vince-exe@users.noreply.github.com>
2022-08-30 21:20:52 +0200

    update

---
+++ TestChat/client_main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <boost/asio.hpp>
+
+int main() {
+    boost::asio::io_service io_service;
+    /* socket creation */
+    boost::asio::ip::tcp::socket socket(io_service);
+    /* connection */
+    socket.connect(boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 8000));
+
+    /* request message from client */
+    const std::string msg = "Hello From Client \n";
+
+    boost::system::error_code er;
+    boost::asio::write(socket, boost::asio::buffer(msg), er);
+
+    if(er) {
+        std::cout << "\n[ FATAL ERROR ]: " << er.message() << std::endl;
+    }
+
+    /* getting response from server */
+    boost::asio::streambuf recvBuffer;
+    boost::asio::read(socket, recvBuffer, boost::asio::transfer_all(), er);
+    
+    if(er && er != boost::asio::error::eof) {
+        std::cout << "\n [ FATAL ERROR ]: " << er.message() << std::endl;
+    }
+    else {
+        const char* data = boost::asio::buffer_cast<const char*>(recvBuffer.data());
+        std::cout << "\nMessage From Server: " << data << std::endl;
+    }
+
+    return 0;
+}
---
+++ TestChat/server_main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <boost/asio.hpp>
+
+std::string read(boost::asio::ip::tcp::socket& socket) {
+    boost::asio::streambuf buffer;
+    boost::asio::read_until(socket, buffer, "\n");
+    std::string data = boost::asio::buffer_cast<const char*>(buffer.data());
+    return data;
+}
+
+void send(boost::asio::ip::tcp::socket& socket, const std::string& message) {
+    const std::string msg = message + "\n";
+    boost::asio::write(socket, boost::asio::buffer(msg));
+}
+
+int main() {
+    boost::asio::io_service service;
+    
+    /* listen for new connections */
+    boost::asio::ip::tcp::acceptor acceptor_(service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 8000));
+    /* create a new socket */   
+    boost::asio::ip::tcp::socket socket(service);
+    /* wait for connection */
+    acceptor_.accept(socket);
+    
+    std::string clientMsg = read(socket);
+    /* read client message */
+    std::cout<<"\n Message From Client: " <<  clientMsg << std::endl;
+
+    /* write client message */
+    send(socket, "Hello From Server");
+
+    return 0;
+}
